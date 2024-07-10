86cb08cfe1aba952d5f923dbbbb20d23a2f72437
myunghoJeong <bwve6g@gmail.com>
2024-07-11 00:22:33 +0900

    using topic

---
+++ calc_ur5Ik/src/set_RT.cpp
@@ -0,0 +1,64 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/float64_multi_array.hpp"
+#include <math.h>
+
+using namespace std::chrono_literals;
+
+double pi = 3.141592;
+
+class set_RT : public rclcpp::Node
+{
+    public:
+    set_RT()
+    : Node("set_rt")
+    {
+        m_publisher = this -> create_publisher<std_msgs::msg::Float64MultiArray>(
+            "target_pos",10);
+        m_timer = this -> create_wall_timer(
+            3s, std::bind(&set_RT::timer_callback, this));
+    }
+    private:
+    double m_x = 0.0;
+    double m_y = 0.6;
+    double m_z = 0.3;
+    double m_roll = pi;
+    double m_pitch = 0.0;
+    double m_yaw = 0.0;
+
+    void timer_callback()
+    {
+        auto message = std_msgs::msg::Float64MultiArray();
+        //message.data = "0.0 0.6 0.3 3.141592 0.0 0.0" ;
+        message.data = {m_x, m_y, m_z, m_roll, m_pitch, m_yaw} ;
+
+        RCLCPP_INFO(this->get_logger(), "x = %f", message.data[0]);
+        RCLCPP_INFO(this->get_logger(), "y = %f", message.data[1]);
+        RCLCPP_INFO(this->get_logger(), "z = %f", message.data[2]);
+        RCLCPP_INFO(this->get_logger(), "roll = %f", message.data[3]);
+        RCLCPP_INFO(this->get_logger(), "pitch = %f", message.data[4]);
+        RCLCPP_INFO(this->get_logger(), "yaw = %f", message.data[5]);
+
+        RCLCPP_INFO(this->get_logger(), "%s", "-----------------");
+        //RCLCPP_INFO(this->get_logger(), "Set RT : %s", message.data.c_str());
+        m_publisher -> publish(message);
+    }
+
+    rclcpp::TimerBase::SharedPtr m_timer;
+    //rclcpp::Publisher<std_msgs::msg::String>::SharedPtr m_publisher;
+    rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr m_publisher;
+    size_t m_count;
+};
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc,argv);
+    rclcpp::spin(std::make_shared<set_RT>());
+    rclcpp::shutdown();
+
+    return 0;
+}
