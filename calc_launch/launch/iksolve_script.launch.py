6d9715e42b46db3b3434bd6505af892d926b0601
myunghoJeong <bwve6g@gmail.com>
2024-08-05 00:01:02 +0900

    1.5

--- calc_launch/launch/iksolve_script.launch.py
+++ calc_launch/launch/iksolve_script.launch.py
@@ -1,10 +1,21 @@
+#!/usr/bin/env python3
+
 import launch
 import launch_ros.actions
 from launch.substitutions import LaunchConfiguration
 
+import os
+from ament_index_python.packages import get_package_share_directory
+
 
 def generate_launch_description():
-    topic_ik = LaunchConfiguration('topic_ik')
+    topic_ik = LaunchConfiguration(
+        'topic_ik',
+        default = os.path.join(
+            get_package_share_directory('topic_ur5ik'),
+            'src',
+            'calc_sub.cpp'))
+
     service_ik = LaunchConfiguration('service_ik')
     action_ik = LaunchConfiguration('action_ik')
 
